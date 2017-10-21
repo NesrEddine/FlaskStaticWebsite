e50e6852ad8592f0a4f791269f67a4b9249b4a77
NesrEddine <nesreddineboudene@gmail.com>
2017-10-21 21:28:01 +0100

    Templates

---
+++ static/js/signin.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+    $('#idSignIn').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+         feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh' 
+        },
+		
+		
+		fields: { 
+			
+		  name: {
+                validators: {
+                        stringLength: {
+                        min: 6,       },
+						
+						notEmpty:{
+							message: 'Please enter your name'
+							}
+				
+							} 
+           		 },
+			
+            password: {
+                validators: {
+                        stringLength: {
+                        min: 6,       }
+				
+							} 
+           		 },
+				 
+				 
+	         confirmpassword: { 
+                validators: {
+                        stringLength: {
+                        min: 6	,       },
+						identical: {
+                    field: 'password',
+                    message: 'The password and its confirm are not the same'
+                }
+				
+							} 
+           		 },
+				 
+				 
+     
+
+		  			 
+			
+		 email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please enter your email address'
+                    },
+                    emailAddress: {
+                        message: 'Please enter a valid email address'
+                    }
+                }
+            }, 
+			
+			
+		 message: {  
+                validators: {
+                        stringLength: {
+                        min: 10,
+						max:150,       },
+				 notEmpty:{
+					 message: 'Please enter your message here'
+					 }
+				
+							} 
+           		 },
+				   
+                  
+            }
+        })
+ 	
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#idSignIn').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target); 
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json'); 
+        });
+});
